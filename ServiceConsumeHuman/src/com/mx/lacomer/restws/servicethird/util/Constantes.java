42a31026cd4f002d090f337bc97733e173555d54
Andres Baez <andresxyk@hotmail.com>
2023-08-01 11:57:22 -0600

    Commit Inicial

---
+++ ServiceConsumeHuman/src/com/mx/lacomer/restws/servicethird/util/Constantes.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.mx.lacomer.restws.servicethird.util;
+
+/**
+ * @author soporte
+ *
+ */
+public class Constantes {
+	public static final String UTF_8 = "UTF-8";
+	public static final String EMPTY = "";
+}
